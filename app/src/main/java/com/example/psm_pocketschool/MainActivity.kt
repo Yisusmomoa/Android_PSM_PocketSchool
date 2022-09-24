e913bd6087968da8b86ae0f84066a9daf1422d87
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2022-09-24 16:55:13 -0500

    se crean las pantallas de signup y login

--- app/src/main/java/com/example/psm_pocketschool/MainActivity.kt
+++ app/src/main/java/com/example/psm_pocketschool/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.psm_pocketschool
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textViewSignUp=findViewById<TextView>(R.id.textViewSignUp)
+        textViewSignUp.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val itemId= v?.id
+        when(itemId){
+            R.id.textViewSignUp->{
+                Log.d("Click", "sign up")
+                val intent=Intent(this, SignUp::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
