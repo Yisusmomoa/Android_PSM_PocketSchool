06bcc624e8745b3b8a37f715269739387e4ae43c
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2022-10-03 01:05:31 -0500

    se crean las pantallas de home, perfil, y se añade el drawerlayout, navigation bar y floating btn

--- app/src/main/java/com/example/psm_pocketschool/Home.kt
+++ app/src/main/java/com/example/psm_pocketschool/Home.kt
@@ -2,10 +2,67 @@ package com.example.psm_pocketschool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.example.psm_pocketschool.databinding.ActivityHomeBinding
+import com.example.psm_pocketschool.databinding.ActivityMainBinding
+import com.example.psm_pocketschool.fragments.*
+import com.example.psm_pocketschool.fragments.Home
 
 class Home : AppCompatActivity() {
+    private lateinit var binding:ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding=ActivityHomeBinding.inflate(layoutInflater)
+
+        //setContentView(R.layout.activity_home) //o
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.search->{
+                    replaceFragment(Search())
+                }
+                R.id.profile->{
+                    replaceFragment(Profile())
+                }
+                R.id.home->{
+                    replaceFragment(Home())
+                }
+                R.id.mistareas->{
+                    replaceFragment(MisTareas())
+                }
+                R.id.misgrupos->{
+                    replaceFragment(Groups())
+                }
+                else->{
+
+                }
+            }
+            true
+        }
+        binding.bottomNavigationView.menu.findItem(R.id.home).setChecked(true)
+
+        //indicamos al controlador que se cambio la toolbar por una nueva
+        val toolbar=findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        val drawer=findViewById<DrawerLayout>(R.id.drawer)
+        setSupportActionBar(toolbar)
+
+        //Icono hamburgesa
+        val toggle=ActionBarDrawerToggle(this, drawer, toolbar, R.string.app_name,R.string.app_name )
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    private fun replaceFragment(fragment:Fragment){
+        val fragmentManager=supportFragmentManager
+        val fragmentTransaction=fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
--- app/src/main/java/com/example/psm_pocketschool/MainActivity.kt
+++ app/src/main/java/com/example/psm_pocketschool/MainActivity.kt
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val textViewSignUp=findViewById<TextView>(R.id.textViewSignUp)
         textViewSignUp.setOnClickListener(this)
+        val btnLogin=findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -21,6 +25,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.textViewSignUp->{
                 Log.d("Click", "sign up")
                 val intent=Intent(this, SignUp::class.java)
+                startActivity(intent)
+            }
+            R.id.btnLogin->{
+                Log.d("Click", "Loginbtn")
+                val intent=Intent(this, Home::class.java)
                 startActivity(intent)
             }
         }
