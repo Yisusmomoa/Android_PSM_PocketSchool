c02af41e4769cd16d578c3ce523cbdb1868f7118
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2022-12-17 09:09:06 -0600

    .

--- app/src/main/java/com/example/psm_pocketschool/core/RetrofitHelper.kt
+++ app/src/main/java/com/example/psm_pocketschool/core/RetrofitHelper.kt
@@ -6,13 +6,13 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitHelper {
     //puro consumo de apis
     //configuración de retrofift
-    //192.168.1.2
+    //http://192.168.1.2:5000/api/
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("http://192.168.1.2:5000/api/")//ruta fija de nuestros endpoints
+            .baseUrl("https://express-psm5.onrender.com/api/")//ruta fija de nuestros endpoints
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
-    // .baseUrl("https://express-psm5.onrender.com")//ruta fija de nuestros endpoints
+    // .baseUrl("https://express-psm5.onrender.com/api/")//ruta fija de nuestros endpoints
 }
